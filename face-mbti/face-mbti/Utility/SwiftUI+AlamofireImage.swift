22956466886ac72ae783b255834021464f7ce852
KYHyeon <run190822@gmail.com>
2021-04-28 23:45:28 +0900

    Alamofire Image 의존성을 SPM으로 변경

--- face-mbti/face-mbti/Utility/SwiftUI+AlamofireImage.swift
+++ face-mbti/face-mbti/Utility/SwiftUI+AlamofireImage.swift
@@ -68,7 +68,7 @@ extension View {
 #if DEBUG
 struct RemoteImage_Previews: PreviewProvider {
     static var previews: some View {
-        RemoteImage(url: URL(string: "https://s3-alpha-sig.figma.com/img/76b7/1117/896a72c6ac7c7d6ffa65a81c26cb0633?Expires=1618790400&Signature=VDzy7AVm4Yj8ZXW3~BAuUW3JxAXjPTW9pqF9XYFE8Wlod098B~~UnPoSQKFs5qGINqdKzCRTmjVF4xr3KdI9rLvhEy4PC5e9lFCaJWle3DlYFt1GNfjvhVUyZP1qpDdJuXiOptkdOHf7YodfaTqXnbCM9buX9b84HRcnTyO8sgew9gKVYQ3cx-D~7T68FK7KNtmHSvHt0~ymYPNNtDZktiLpLbX-KIhbpPHrpb2SgOXRgzHswiZPr4a1R8uwVvZau-CdRI43KpgUHpBaInWE69Fev9qK8QtqwSAahqkWfhOoT8QUG72jQrbW5dE-3h2UyQpx1vz-g~F67CNbrUNWZw__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA")!)
+        RemoteImage(url: URL(string: "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png")!)
     }
 }
 #endif
